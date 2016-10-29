90bf90083577f3240939d88aa488e0dd895090bf
Nuri Uzunoğlu <nuriu@users.noreply.github.com>
2016-10-29 18:58:37 +0300

    Add project files.

---
+++ SatrancAt/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace SatrancAt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.lblSecim = new System.Windows.Forms.Label();
+            this.lblSecilebilir = new System.Windows.Forms.Label();
+            this.lblSkor = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.radioButton5);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(554, 47);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Grid Boyutu";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(466, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Oluştur";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(375, 20);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(48, 17);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "9 x 9";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(283, 20);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(48, 17);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "8 x 8";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(191, 20);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(48, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "7 x 7";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(99, 20);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(48, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "6 x 6";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 20);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(48, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "5 x 5";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // lblSecim
+            // 
+            this.lblSecim.AutoSize = true;
+            this.lblSecim.BackColor = System.Drawing.Color.DarkBlue;
+            this.lblSecim.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSecim.ForeColor = System.Drawing.Color.White;
+            this.lblSecim.Location = new System.Drawing.Point(575, 92);
+            this.lblSecim.Name = "lblSecim";
+            this.lblSecim.Size = new System.Drawing.Size(89, 20);
+            this.lblSecim.TabIndex = 1;
+            this.lblSecim.Text = "Aktif Seçim";
+            // 
+            // lblSecilebilir
+            // 
+            this.lblSecilebilir.AutoSize = true;
+            this.lblSecilebilir.BackColor = System.Drawing.Color.Red;
+            this.lblSecilebilir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSecilebilir.ForeColor = System.Drawing.Color.White;
+            this.lblSecilebilir.Location = new System.Drawing.Point(574, 118);
+            this.lblSecilebilir.Name = "lblSecilebilir";
+            this.lblSecilebilir.Size = new System.Drawing.Size(104, 20);
+            this.lblSecilebilir.TabIndex = 2;
+            this.lblSecilebilir.Text = "Seçilebilir Yer";
+            // 
+            // lblSkor
+            // 
+            this.lblSkor.AutoSize = true;
+            this.lblSkor.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSkor.Location = new System.Drawing.Point(573, 27);
+            this.lblSkor.Name = "lblSkor";
+            this.lblSkor.Size = new System.Drawing.Size(56, 25);
+            this.lblSkor.TabIndex = 3;
+            this.lblSkor.Text = "Skor";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.lblSkor);
+            this.Controls.Add(this.lblSecilebilir);
+            this.Controls.Add(this.lblSecim);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "132805010 - Nuri UZUNOĞLU";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label lblSecim;
+        private System.Windows.Forms.Label lblSecilebilir;
+        private System.Windows.Forms.Label lblSkor;
+    }
+}
+
---
+++ SatrancAt/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SatrancAt
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Skorumuz
+        /// </summary>
+        private int skor = 0;
+
+        /// <summary>
+        /// Bir önceki seçim.
+        /// </summary>
+        private Label oncekiSecim = null;
+        /// <summary>
+        /// Bir önceki seçimin arkaplan rengi.
+        /// </summary>
+        private Color oncekiSecimArkaplanRengi;
+        /// <summary>
+        /// Bir önceki seçimin metin rengi.
+        /// </summary>
+        private Color oncekiSecimMetinRengi;
+
+        /// <summary>
+        /// Daha önce ziyaret edilmiş yerler.
+        /// </summary>
+        private List<Label> secilmisYerler;
+
+        /// <summary>
+        /// Anlık seçilebilecek yerler.
+        /// </summary>
+        private List<Label> secilebilirYerler;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Açılışta skoru yazdırıyoruz ve listelerizi oluşturuyoruz.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblSkor.Text = "Skor: " + skor.ToString();
+            secilmisYerler = new List<Label>();
+            secilebilirYerler = new List<Label>();
+        }
+
+        /// <summary>
+        /// Oluşturma tuşuna tıklandığında grid boyutuna karar verip ilgili metodu çağırıyoruz.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int gridKenar = -1;
+
+            #region Grid Boyut Seçimi
+            if (radioButton1.Checked)
+            {
+                gridKenar = 5;
+            }
+            else if (radioButton2.Checked)
+            {
+                gridKenar = 6;
+            }
+            else if (radioButton3.Checked)
+            {
+                gridKenar = 7;
+            }
+            else if (radioButton4.Checked)
+            {
+                gridKenar = 8;
+            }
+            else if (radioButton5.Checked)
+            {
+                gridKenar = 9;
+            }
+            else
+            {
+                MessageBox.Show("Grid boyutunu seçmediniz!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            #endregion
+
+            if (gridKenar != -1)
+            {
+                //MessageBox.Show(gridKenar + " x " + gridKenar + " boyutunda grid oluşturuluyor.",
+                //                    "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                button1.Enabled = false;
+                GridOlustur(gridKenar);
+            }
+        }
+
+        /// <summary>
+        /// Hücre sayısına göre gridi oluşturuyoruz.
+        /// </summary>
+        /// <param name="hucre">Oluşturulacak gridin hücre sayısı.</param>
+        private void GridOlustur(int hucre)
+        {
+            for (int i = 0; i < hucre; i++)
+            {
+                for (int j = 0; j < hucre; j++)
+                {
+                    Label l = new Label();
+
+                    #region Pozisyon İşlemleri
+                    l.Width = 50;
+                    l.Height = 50;
+
+                    l.Left = 30 + (i * 50);
+                    l.Top = 75 + (j * 50);
+
+                    l.Text = (j + 1) + (i + 1).ToString();
+                    l.TextAlign = ContentAlignment.MiddleCenter;
+                    #endregion
+
+                    #region Görsel İşlemler
+                    l.BorderStyle = BorderStyle.FixedSingle;
+
+                    if ((i + j) % 2 == 0)
+                    {
+                        l.BackColor = Color.Beige;
+                    }
+                    else
+                    {
+                        l.BackColor = Color.Wheat;
+                    }
+                    #endregion
+
+                    l.Click += HamleleriGoster;
+
+                    this.Controls.Add(l);
+
+                    // İlk hamle için her yeri seçilebilir yapıyoruz.
+                    secilebilirYerler.Add(l);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Bir pozisyon seçildiğinde, seçilebilir yerleri gösteriyoruz.
+        /// </summary>
+        /// <param name="sender">Seçilen pozisyon.</param>
+        /// <param name="e">Olay argümanları.</param>
+        private void HamleleriGoster(object sender, EventArgs e)
+        {
+            // Skoru arttırıyoruz.
+            skor++;
+            lblSkor.Text = "Skor: " + skor.ToString();
+
+            // Seçilebilir yerleri sıfırlıyoruz.
+            secilebilirYerler = new List<Label>();
+
+            // TODO: Seçilebilir mi kontrol et. Skor 1 üzerindeyse. (En az 1 seçim yaptıysa).
+
+            if (oncekiSecim == ((Label)sender))
+            {
+                MessageBox.Show("Aynı elemanı tekrar seçemezsiniz.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                #region Renk İşlemleri
+                if (oncekiSecim != null)
+                {
+                    oncekiSecim.BackColor = oncekiSecimArkaplanRengi;
+                    oncekiSecim.ForeColor = oncekiSecimMetinRengi;
+                }
+
+                oncekiSecim = ((Label)sender);
+                oncekiSecimArkaplanRengi = ((Label)sender).BackColor;
+                oncekiSecimMetinRengi = ((Label)sender).ForeColor;
+
+                ((Label)sender).BackColor = Color.DarkBlue;
+                ((Label)sender).ForeColor = Color.White;
+                #endregion
+
+                lblSecim.Text = "Aktif Seçim: " + ((Label)sender).Text;
+
+                int secilenKod = Convert.ToInt16(((Label)sender).Text);
+
+                // Seçilebilir kodları seçilen koda göre belirle.
+                int[] secilebilirKodlar = new int[8];
+                secilebilirKodlar[0] = secilenKod - 21;
+                secilebilirKodlar[1] = secilenKod - 19;
+                secilebilirKodlar[2] = secilenKod + 19;
+                secilebilirKodlar[3] = secilenKod + 21;
+                secilebilirKodlar[4] = secilenKod - 12;
+                secilebilirKodlar[5] = secilenKod - 8;
+                secilebilirKodlar[6] = secilenKod + 8;
+                secilebilirKodlar[7] = secilenKod + 12;
+
+                secilmisYerler.Add((Label)sender);
+                secilebilirYerler.Remove((Label)sender);
+                
+                lblSecilebilir.Text = "Seçilebilir Yerler: \n";
+
+                foreach (var item in Controls)
+                {
+                    if (item is Label)
+                    {
+                        foreach (var kod in secilebilirKodlar)
+                        {
+                            if (((Label)item).Text == kod.ToString())
+                            {
+                                lblSecilebilir.Text += ((Label)item).Text + "\n";
+
+                                secilebilirYerler.Add((Label)item);
+
+                                // TODO: KÖŞELER SEÇİLDİĞİNDE HATALI BOYUYOR DÜZELT!
+                                ((Label)item).BackColor = Color.Red;
+                                ((Label)item).ForeColor = Color.White;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
